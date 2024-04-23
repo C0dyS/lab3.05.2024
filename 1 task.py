e05318655a088b8d790de00ee3fa6e953b48614c
C0dyS <sashaculle@gmail.com>
2024-04-23 20:37:23 +0200

    3 task done

--- 1 task.py
+++ 1 task.py
@@ -2,36 +2,42 @@ import pickle
 import gzip
 
 def main():
+    workable_data = {}
     while True :
         user_choice = int(input('''
-        1 - export data
-        2 - import data
-        3 - add data
-        4 - remove data
-        5 - to exit
+        1 - add data
+        2 - remove data
+        3 - search(passwords for logins)
+        4 - edit
+        5 - import data
+        6 - save data
+        7 - exit
         '''))
+
         if user_choice == 1 :
-            with gzip.open('task_file_2.gz','wb') as file:
-                exported_data = list(input())
-                pickle.dump(exported_data,file)
+                new_login, new_password = input("Enter new login and password separated by space: ").split()
+                workable_data[new_login] = new_password
         elif user_choice == 2:
-            with gzip.open('task_file_2.gz','rb') as file:
-                imported_data = pickle.load(file)
+            user_remove_choice = input('Enter login value to remove')
+            if user_remove_choice in workable_data:
+                workable_data.pop(user_remove_choice)
         elif user_choice == 3:
-            with gzip.open('task_file_2.gz', 'rb') as file:
-                temporal_data = pickle.load(file)
-            temporal_data.append(input())
-            with gzip.open('task_file_2.gz', 'wb') as file:
-                pickle.dump(temporal_data, file)
+            user_password_search = input('Enter login to find out corresponding password')
+            if user_password_search in workable_data:
+                print(workable_data[user_password_search])
+            else:
+                print('such login doesnt exit')
         elif user_choice == 4:
-            with gzip.open('task_file_2.gz', 'rb') as file1:
-                temporal_data = pickle.load(file1)
-                user_remove_choice = int(input('select amount of elements to remove from the back: '))
-                if user_remove_choice > 0 and user_remove_choice <= len(temporal_data):
-                    temporal_data = temporal_data[:-user_remove_choice]
-                    with gzip.open('task_file_2.gz', 'wb') as file:
-                        pickle.dump(temporal_data, file)
-                else:
-                    print("Invalid choice. There are not enough elements to remove.")
+            user_login,user_password_change = input('Enter your login and new password separated by space').split()
+            workable_data[user_login] = user_password_change
+        elif user_choice == 5:
+            file_path = input('enter file path')
+            with open(file_path, 'rb') as file:
+                workable_data = pickle.load(file)
+        elif user_choice == 6:
+            file_path = input('Enter file path to save data: ')
+            with gzip.open(file_path, 'wb') as file:
+                pickle.dump(workable_data, file)
         else:
+            print('exiting the program')
             break
