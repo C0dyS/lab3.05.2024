95bbfa9dccff5e0dcefc2d842333c5a361d7ae49
C0dyS <sashaculle@gmail.com>
2024-04-23 20:04:02 +0200

    2 task done

--- 1 task.py
+++ 1 task.py
@@ -1,15 +1,37 @@
 import pickle
+import gzip
 
-def input_to_file():
-    start_list = list(input())
-    with open('task_file.txt','wb') as file:
-        pickle.dump(start_list,file)
-
-def file_to_code():
-    with open('task_file.txt','rb') as file:
-        imported_data = pickle.load(file)
-    print(type(imported_data),imported_data)
-
-
-input_to_file()
-file_to_code()
+def main():
+    while True :
+        user_choice = int(input('''
+        1 - export data
+        2 - import data
+        3 - add data
+        4 - remove data
+        5 - to exit
+        '''))
+        if user_choice == 1 :
+            with gzip.open('task_file_2.gz','wb') as file:
+                exported_data = list(input())
+                pickle.dump(exported_data,file)
+        elif user_choice == 2:
+            with gzip.open('task_file_2.gz','rb') as file:
+                imported_data = pickle.load(file)
+        elif user_choice == 3:
+            with gzip.open('task_file_2.gz', 'rb') as file:
+                temporal_data = pickle.load(file)
+            temporal_data.append(input())
+            with gzip.open('task_file_2.gz', 'wb') as file:
+                pickle.dump(temporal_data, file)
+        elif user_choice == 4:
+            with gzip.open('task_file_2.gz', 'rb') as file1:
+                temporal_data = pickle.load(file1)
+                user_remove_choice = int(input('select amount of elements to remove from the back: '))
+                if user_remove_choice > 0 and user_remove_choice <= len(temporal_data):
+                    temporal_data = temporal_data[:-user_remove_choice]
+                    with gzip.open('task_file_2.gz', 'wb') as file:
+                        pickle.dump(temporal_data, file)
+                else:
+                    print("Invalid choice. There are not enough elements to remove.")
+        else:
+            break
